c2740611f4824da1d03c73ef67101e3e03d82965
Druesne Morgan <morgan.druesne@epitech.eu>
2020-02-13 09:51:20 +0100

    ..

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** getline
+** File description:
+** getline
+*/
+
+#ifndef  READ_SIZE
+#    define  READ_SIZE (6)
+#endif
+
+#ifndef MY_getline_
+#define MY_getline_
+
+char *get_next_line(int fd);
+
+#endif
